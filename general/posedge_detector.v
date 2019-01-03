855fbdd5cbd044e865e30a47f0e280de7e491986
knknkn1162 <knknkn1162@gmail.com>
2019-01-03 11:17:27 +0900

    improve posedge detector

--- general/posedge_detector.v
+++ general/posedge_detector.v
@@ -2,8 +2,7 @@
 `define _posedge_detector
 
 module posedge_detector (
-  input wire clk, i_sclr,
-  input wire i_en,
+  input wire clk, i_sclr, i_en, i_dat,
   output wire o_en_rise
 );
 
@@ -12,12 +11,12 @@ module posedge_detector (
   always @(posedge clk) begin
     if (i_sclr) begin
       r_en <= 3'b000;
-    end else begin
-      r_en <= {r_en[1:0], i_en};
+    end else if (i_en) begin
+      r_en <= {r_en[1:0], i_dat};
     end
   end
 
-  assign o_en_rise = (r_en[2:1] == 2'b01);
+  assign o_en_rise = (r_en[2:1] == 2'b01) & i_en;
 endmodule
 
 `endif
--- general/posedge_detector_tb.v
+++ general/posedge_detector_tb.v
@@ -2,7 +2,7 @@
 `include "posedge_detector.v"
 
 module posedge_detector_tb;
-  reg clk, i_sclr, i_en;
+  reg clk, i_sclr, i_en, i_dat;
   wire o_en_rise;
 
   parameter CLK_PERIOD = 10;
@@ -11,6 +11,7 @@ module posedge_detector_tb;
     .clk(clk),
     .i_sclr(i_sclr),
     .i_en(i_en),
+    .i_dat(i_dat),
     .o_en_rise(o_en_rise)
   );
 
@@ -23,12 +24,12 @@ module posedge_detector_tb;
     @(posedge clk) #1
       i_sclr = 1'b0;
       `assert_eq(o_en_rise, 1'b0);
-      i_en = 1'b1;
+      i_en = 1'b1; i_dat = 1'b1;
     @(posedge clk) #1
       `assert_eq(o_en_rise, 1'b0);
     @(posedge clk) #1
       `assert_eq(o_en_rise, 1'b1);
-      i_en = 1'b0;
+      i_dat = 1'b0;
     @(posedge clk) #1
       `assert_eq(o_en_rise, 1'b0);
     @(posedge clk) #1
