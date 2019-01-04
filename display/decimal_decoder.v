584843f96ca6c9feec918a65c6f960814a941802
knknkn1162 <knknkn1162@gmail.com>
2019-01-04 21:13:51 +0900

    bugfix decimal & hex_decoder

--- display/decimal_decoder.v
+++ display/decimal_decoder.v
@@ -10,8 +10,7 @@ module decimal_decoder (
     case (i_num)
       4'd0: o_seg7 <= 7'b100_0000;
       4'd1: o_seg7 <= 7'b111_1001;
-      4'd2: o_seg7 <= 7'b100_0100;
-      4'd3: o_seg7 <= 7'b011_0000;
+      4'd2: o_seg7 <= 7'b010_0100;
       4'd3: o_seg7 <= 7'b011_0000;
       4'd4: o_seg7 <= 7'b001_1001;
       4'd5: o_seg7 <= 7'b001_0010;
