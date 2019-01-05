7dadb0605f4d4878aca0d1d1df01c1bd987913cc
knknkn1162 <knknkn1162@gmail.com>
2019-01-05 14:00:54 +0900

    impl enableN_gen and pass its testbench

---
+++ general/enableN_gen.v
@@ -0,0 +1,23 @@
+`ifndef _enableN_gen
+`define _enableN_gen
+
+`include "counterN_en.v"
+
+module enableN_gen #(
+  parameter ULIMIT = 10,
+  parameter WIDTH = 4
+) (
+  input wire clk, i_sclr,
+  output wire o_en
+);
+
+  wire [WIDTH-1:0] s_cnt;
+  counterN_en #(ULIMIT, WIDTH) counterN_en0 (
+    .clk(clk), .i_sclr(i_sclr), .i_en(1'b1),
+    .o_cnt(s_cnt)
+  );
+
+  assign o_en = (s_cnt == {WIDTH{1'b0}}) ? 1'b1 : 1'b0;
+endmodule
+
+`endif
---
+++ general/enableN_gen_tb.v
@@ -0,0 +1,39 @@
+`include "testbench.v"
+`include "enableN_gen.v"
+
+module enableN_gen_tb;
+  parameter ULIMIT = 10;
+  parameter WIDTH = 4;
+  reg clk, i_sclr;
+  wire o_en;
+
+  parameter CLK_PERIOD = 10;
+
+  enableN_gen #(ULIMIT, WIDTH) uut (
+    .clk(clk),
+    .i_sclr(i_sclr),
+    .o_en(o_en)
+  );
+
+  `dump_block
+  `conf_clk_block(clk, CLK_PERIOD)
+
+  initial begin
+    #(CLK_PERIOD)
+      i_sclr = 1'b1;
+    @(posedge clk) #1
+      i_sclr = 1'b0;
+      `assert_eq(o_en, 1'b1); // 0
+    @(posedge clk) #1
+      `assert_eq(o_en, 1'b0); // 1
+    repeat(8) @(posedge clk) #1
+      `assert_eq(o_en, 1'b0); // 9
+    @(posedge clk) #1
+      `assert_eq(o_en, 1'b1); // 0
+    @(posedge clk) #1
+      `assert_eq(o_en, 1'b0);
+    @(posedge clk) #1
+      `assert_eq(o_en, 1'b0);
+    `end_tb
+  end
+endmodule
