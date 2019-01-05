7cdfe6406a922b302d9ed0d2ef372df5dc633d78
knknkn1162 <knknkn1162@gmail.com>
2019-01-05 15:01:36 +0900

    draw rectanble in gen_640_480 module

--- vga/gen_640_480.v
+++ vga/gen_640_480.v
@@ -7,20 +7,19 @@
 module gen_640_480 (
   input wire clk, i_sclr, i_px_clk,
   input wire i_hsync_en, i_vsync_en,
-  input i_haddr_en, i_vaddr_en,
-  input [9:0] i_hidx,
+  input wire i_haddr_en, i_vaddr_en,
+  input wire [9:0] i_hidx,
+  input wire [8:0] i_vidx,
   output wire [3:0] o_vga_red, o_vga_green, o_vga_blue
 );
 
   wire [11:0] s_color0, s_color1;
-
-  wire [COLOR_BIT_SIZE-1:0] s_idx; // 307200
-  localparam COLOR_BIT_SIZE = 19;
-  localparam ULIMIT = 19'd307200;
-
+  wire s_area_en;
   localparam RED = 12'hF00;
   localparam BLACK = 12'h000;
-  assign s_color0 = (i_haddr_en & (i_hidx >= 10'd100 && i_hidx < 10'd200)) ? RED : BLACK;
+
+  assign s_area_en = (i_haddr_en & (i_hidx >= 10'd100 && i_hidx < 10'd200)) & (i_vaddr_en & (i_vidx >= 9'd100 && i_vidx < 9'd200));
+  assign s_color0 = (s_area0) ? RED : BLACK;
 
   mux2 #(12) mux2_0(
     .i_d0(12'h000),
