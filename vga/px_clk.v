0324fc46d4f7d2f3830f2357b49618e47e2b8309
knknkn1162 <knknkn1162@gmail.com>
2019-01-05 14:59:01 +0900

    add o_ven in px_clk

--- vga/px_clk.v
+++ vga/px_clk.v
@@ -5,14 +5,21 @@
 
 module px_clk (
   input wire clk, i_sclr,
-  output wire o_px_clk
+  output wire o_px_clk,
+  output wire o_ven
 );
 
+  localparam WIDTH = 800;
   toggle #(1'b0) toggle0 (
     .clk(clk),
     .i_sclr(i_sclr),
     .i_en(1'b1),
     .o_sw(o_px_clk)
+  );
+
+  enableN_gen #(WIDTH*2, 11) (
+    .clk(clk), .i_sclr(i_sclr),
+    .o_en(o_ven)
   );
 
 endmodule
